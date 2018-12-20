b9af070f487a052a8b77e404d0834953a62ea256
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2018-12-20 12:12:12 +0200

    add more test

--- src/test/scala/http/MsgHandlerTest.scala
+++ src/test/scala/http/MsgHandlerTest.scala
@@ -6,6 +6,7 @@ import java.time.LocalDateTime
 import com.sun.net.httpserver.HttpExchange
 import core.{InitPaymentMessage, InitPayments, Money}
 import keys.KeysFileOps
+import org.apache.http.HttpStatus
 import org.mockito.Matchers
 import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.FlatSpec
@@ -36,6 +37,32 @@ class MsgHandlerTest extends FlatSpec with org.scalatest.Matchers with MockitoSu
       Matchers.eq("Initial payment message verified."))
 
     initPayments.initPayments.containsValue(signedMessage) shouldBe true
+  }
+
+  "Message handler" should "reject payment message during failed verification" in {
+
+    val mockExchange = mock[HttpExchange]
+    val mockBcHttpServer = mock[BCHttpServer]
+    val initPayments = new InitPayments()
+    val keysFileOps = mock[KeysFileOps]
+
+    when(keysFileOps.readKeyFromFile("Igor/privateKey")).thenReturn("MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCC94HoY839pqOB/m2D00X4+8vsM6kzUby8gk7Eq8XVsgw==")
+    when(keysFileOps.readKeyFromFile("Igor/publicKey")).thenReturn("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEDibd8O5I928ZnTU7RYTy6Od3K3SrGlC+V8lkMYrdJuzT9Ig/Iq8JciaukxCYmVSO1mZuC65xMkxSb5Q0rNZ8og==")
+
+    val signedMessage = InitPaymentMessage.apply("Igor", "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEDibd8O5I928ZnTU7RYTy6Od3K3SrGlC+V8lkMYrdJuzT9Ig/Iq8JciaukxCYmVSO1mZuC65xMkxSb5Q0rNZ8og==",
+      "(publicKeyTo)", Money("EUR", 2025), LocalDateTime.now, keysFileOps)
+    val temperedMessage = signedMessage.copy(money = Money("EUR", 202500))
+    val is = new ByteArrayInputStream(temperedMessage.serialize.getBytes)
+
+    when(mockExchange.getRequestMethod).thenReturn("POST")
+    when(mockExchange.getRequestBody).thenReturn(is)
+
+    new MsgHandler(mockBcHttpServer, initPayments, keysFileOps).handle(mockExchange)
+
+    verify(mockBcHttpServer, times(1)).sendHttpResponse(Matchers.eq(mockExchange), Matchers.eq(HttpStatus.SC_BAD_REQUEST),
+      Matchers.eq("Initial payment message validation failed."))
+
+    initPayments.initPayments.containsValue(signedMessage) shouldBe false
   }
 
 }
