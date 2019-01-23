85751fcf5abdeff3bfe97e6f8a12a4eeb0d66bd0
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2019-01-23 17:00:43 +0200

    implement selection of trusted users i users handler

--- src/main/scala/http/UsersHandler.scala
+++ src/main/scala/http/UsersHandler.scala
@@ -3,28 +3,86 @@ package http
 import java.io.IOException
 
 import com.sun.net.httpserver.{HttpExchange, HttpHandler}
-import core.BlockChain
+import core.{BlockChain, Statement}
+import keys.{KeysFileOps, KeysSerializator}
+import org.apache.http.HttpStatus.SC_BAD_REQUEST
 import org.slf4j.{Logger, LoggerFactory}
-import statements.RegisteredUser
-import util.StringConverter
+import statements.{ApprovedFact, RegisteredUser}
+import util.{DateTimeUtil, HttpUtil, StringConverter}
 
-class UsersHandler(bcHttpServer: BCHttpServer, bc: BlockChain) extends HttpHandler with StringConverter {
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class UsersHandler(nodeName: String, bcHttpServer: BCHttpServer, bc: BlockChain, val keysFileOps: KeysFileOps) extends HttpHandler with StringConverter
+  with DateTimeUtil with HttpUtil with KeysSerializator {
 
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
   @throws[IOException]
   def handle(exchange: HttpExchange): Unit = {
-    val facts = bc.blocksFrom(0) flatMap { block =>
-      bc.extractFact(block) match {
-        case Right(fact) if fact.statement.isInstanceOf[RegisteredUser] => Some(fact.statement.toString)
-        case Right(fact) => None
-        case Left(_) =>
-          logger.error(s"Cannot extract fact: ${new String(block.data)}")
+
+    val mayBeQuery = Option(exchange.getRequestURI.getQuery)
+    getRequestParam(mayBeQuery, "trusted") match {
+      case Some(trusted) if trusted.toBoolean => respondWithTrustedUsers(exchange, getRequestParam(mayBeQuery, "forUser"))
+      case _ => respondWithAllUsers(exchange)
+    }
+
+  }
+
+  def respondWithAllUsers(exchange: HttpExchange) = {
+    val users = forEachBlock { (statement, hash) =>
+      statement match {
+        case user: RegisteredUser => Some(user)
+        case _ => None
+      }
+    }
+
+    bcHttpServer.sendHttpResponse(exchange, combine(users))
+  }
+
+  def respondWithTrustedUsers(exchange: HttpExchange, mayBeForUser: Option[String]) = {
+    mayBeForUser match {
+      case Some(forUser) =>
+        if (!keysFileOps.isKeysDirExists(nodeName, forUser)) {
+          bcHttpServer.sendHttpResponse(exchange, SC_BAD_REQUEST, s"User ${forUser} doesn't exist")
+        } else {
+          val approvedUsers = readApprovedUsers(forUser)
+          bcHttpServer.sendHttpResponse(exchange, combine(approvedUsers))
+        }
+      case None =>
+        bcHttpServer.sendHttpResponse(exchange, SC_BAD_REQUEST, "User name should be specified in request query (forUser)")
+    }
+  }
+
+  private def readApprovedUsers(forUser: String) = {
+    val users = mutable.HashMap[String, RegisteredUser]()
+    val userApprovals = ListBuffer[ApprovedFact]()
+
+    val approverPublicKey = readPublicKey(nodeName, forUser)
+    forEachBlock { (statement, registeredUserFactHash) =>
+      statement match {
+        case user: RegisteredUser => users.put(registeredUserFactHash, user)
+        case approvedFact: ApprovedFact if approvedFact.approverPublicKey == approverPublicKey => userApprovals.append(approvedFact)
+        case _ => // none
+      }
+      None
+    }
+    users.filter { case (registeredUserFactHash, registeredUser) =>
+      registeredUser.publicKey == approverPublicKey || userApprovals.find(_.factHash == registeredUserFactHash).nonEmpty
+    }.values.toSeq.sortBy(_.timestamp)
+  }
+
+  private def forEachBlock[T](processStatement: (Statement, String) => Option[T]) = {
+    bc.blocksFrom (0) flatMap { block =>
+      bc.extractFact (block) match {
+        case Right (fact) => processStatement(fact.statement, block.factHash)
+        case Left (_) =>
+          logger.error (s"Cannot extract fact: ${new String (block.data)}")
           None
       }
-    } mkString "\n"
-
-    bcHttpServer.sendHttpResponse(exchange, facts)
+    }
   }
+
+  private def combine(users: Seq[RegisteredUser]) = users mkString "\n"
 }
 
--- src/test/scala/http/UsersHandlerTest.scala
+++ src/test/scala/http/UsersHandlerTest.scala
@@ -1,20 +1,19 @@
 package http
 
+import java.net.URI
 import java.time.LocalDateTime
 
 import business.Money
 import com.sun.net.httpserver.HttpExchange
-import core.{BlockChain, Fact, TestBlockChain}
+import core.{BlockChain, Statement, TestBlockChain}
 import keys.KeysFileOps
 import messages.SignedStatementMessage
 import org.mockito.Matchers
 import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.FlatSpec
 import org.scalatest.mockito.MockitoSugar
-import statements.{Payment, RegisteredUser}
+import statements.{ApprovedFact, Payment, RegisteredUser}
 import util.StringConverter
-import json.JsonSerializer
-import json.FactJson._
 
 class UsersHandlerTest extends FlatSpec with org.scalatest.Matchers with MockitoSugar with StringConverter {
   "UsersHandler" should "return all users on request" in {
@@ -23,28 +22,37 @@ class UsersHandlerTest extends FlatSpec with org.scalatest.Matchers with Mockito
     val blockChain = new TestBlockChain
     val keysFileOps = mock[KeysFileOps]
 
-    val publicKey1 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEDibd8O5I928ZnTU7RYTy6Od3K3SrGlC+V8lkMYrdJuzT9Ig/Iq8JciaukxCYmVSO1mZuC65xMkxSb5Q0rNZ8og=="
+    val (user1, user2, user3) = addTestData(blockChain)
+    blockChain.size shouldBe 9
+
+    when(mockExchange.getRequestURI).thenReturn(new URI("/users"))
+
+    new UsersHandler("Riga", mockBcHttpServer, blockChain, keysFileOps).handle(mockExchange)
+
+    verify(mockBcHttpServer, times(1)).sendHttpResponse(Matchers.eq(mockExchange), Matchers.eq(
+      s"""$user1
+         |$user2
+         |$user3"""
+        .stripMargin))
+
+  }
+
+  it should "return only trusted users for given user" in {
+    val mockExchange = mock[HttpExchange]
+    val mockBcHttpServer = mock[BCHttpServer]
+    val blockChain = new TestBlockChain
+    val keysFileOps = mock[KeysFileOps]
+
     val publicKey2 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEp0qOMxie16K1oArb+FGKB6YSbl+Hz3pLsVI4r6zWMXmtuD6QFZxGDhbvPO6c969SFEW5VmOSelb8ck+2TysK/Q=="
 
-    val user1 = RegisteredUser("Igor", "ipcholkin@gmail.com", publicKey1)
-    addUser(user1, blockChain)
-    val factHash1 = blockChain.getLatestBlock.factHash
+    val (user1, user2, user3) = addTestData(blockChain)
+    blockChain.size shouldBe 9
 
-    blockChain.add(blockChain.genNextBlock("randomFact".getBytes))
+    when(mockExchange.getRequestURI).thenReturn(new URI("/users?trusted=true&forUser=John"))
+    when(keysFileOps.isKeysDirExists("Riga", "John")).thenReturn(true)
+    when(keysFileOps.readKeyFromFile("Riga", "John", "publicKey")).thenReturn(publicKey2)
 
-    val user2 = RegisteredUser("John", "john@gmail.com", publicKey2)
-    addUser(user2, blockChain)
-    val factHash2 = blockChain.getLatestBlock.factHash
-
-    blockChain.add(blockChain.genNextBlock("randomFact2".getBytes))
-
-    addPayment(Payment.verifyAndCreate("Riga", publicKey1, publicKey2, Money("EUR", 2025), LocalDateTime.of(2018, 12, 1, 15, 0)).right.get, blockChain)
-
-    addPayment(Payment.verifyAndCreate("Riga", publicKey1, publicKey2, Money("EUR", 3035), LocalDateTime.of(2019, 1, 6, 12, 5)).right.get, blockChain)
-
-    blockChain.size shouldBe 7
-
-    new UsersHandler(mockBcHttpServer, blockChain).handle(mockExchange)
+    new UsersHandler("Riga", mockBcHttpServer, blockChain, keysFileOps).handle(mockExchange)
 
     verify(mockBcHttpServer, times(1)).sendHttpResponse(Matchers.eq(mockExchange), Matchers.eq(
       s"""$user1
@@ -53,17 +61,39 @@ class UsersHandlerTest extends FlatSpec with org.scalatest.Matchers with Mockito
 
   }
 
-  def addUser(registeredUser: RegisteredUser, bc: BlockChain) = {
-    val signedStatement = SignedStatementMessage(registeredUser, Nil)
-    val fact = Fact(signedStatement.statement, Nil)
-    val serializedFact = JsonSerializer.serialize(fact).getBytes
-    val block = bc.genNextBlock(serializedFact)
-    bc.add(block)
+  def addTestData(blockChain: BlockChain) = {
+    val publicKey1 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEDibd8O5I928ZnTU7RYTy6Od3K3SrGlC+V8lkMYrdJuzT9Ig/Iq8JciaukxCYmVSO1mZuC65xMkxSb5Q0rNZ8og=="
+    val publicKey2 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEp0qOMxie16K1oArb+FGKB6YSbl+Hz3pLsVI4r6zWMXmtuD6QFZxGDhbvPO6c969SFEW5VmOSelb8ck+2TysK/Q=="
+    val publicKey3 = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEHT4R4zS0wZlpfUwrocaNM1vYHhjXDE1lZ5vKzaZFEjcN2GH2SRdFm/PmNlcXpEt7g8/FQdgMVA46cWbHUZDQsg=="
+
+    val user1 = RegisteredUser("Igor", "ipcholkin@gmail.com", publicKey1)
+    addSignedFact(user1, blockChain)
+    val factHash1 = blockChain.getLatestBlock.factHash
+
+    blockChain.add(blockChain.genNextBlock("randomFact".getBytes))
+
+    val user2 = RegisteredUser("John", "john@gmail.com", publicKey2)
+    addSignedFact(user2, blockChain)
+    val factHash2 = blockChain.getLatestBlock.factHash
+
+    blockChain.add(blockChain.genNextBlock("randomFact2".getBytes))
+
+    addSignedFact(Payment.verifyAndCreate("Riga", publicKey1, publicKey2, Money("EUR", 2025), LocalDateTime.of(2018, 12, 1, 15, 0)).right.get, blockChain)
+
+    addSignedFact(Payment.verifyAndCreate("Riga", publicKey1, publicKey2, Money("EUR", 3035), LocalDateTime.of(2019, 1, 6, 12, 5)).right.get, blockChain)
+
+    val user3 = RegisteredUser("Levi Strauss", "levis@gmail.com", publicKey3)
+    addSignedFact(user3, blockChain)
+    val factHash3 = blockChain.getLatestBlock.factHash
+
+    val approvedUser = ApprovedFact(factHash1, publicKey2)
+    addSignedFact(approvedUser, blockChain)
+
+    (user1, user2, user3)
   }
 
-  def addPayment(payment: Payment, bc: BlockChain) = {
-    val signedStatement1 = SignedStatementMessage(payment, Nil)
+  def addSignedFact(statement: Statement, bc: BlockChain) = {
+    val signedStatement1 = SignedStatementMessage(statement, Nil)
     bc.addFactToNewBlock(signedStatement1)
-    val paymentHash1 = bc.getLatestBlock.factHash
   }
 }
